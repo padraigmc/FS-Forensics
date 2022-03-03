edf2001778fffa71436ddd0523169908a3bb3295
Pádraig McCarthy <padraig.mc1999@gmail.com>
2022-03-03 19:00:15 +0000

    NTFS - added NTFSAttribute class to get attribute information given the path to the disk image and the byte offset to the attribute

--- NTFS.py
+++ NTFS.py
@@ -2,28 +2,69 @@ from phase_1 import BinaryFile
 
 
 class NTFS:
-    def __init__(self, file: BinaryFile, start_sector):
+    def __init__(self, file_path, start_sector):
+        file = BinaryFile(file_path)
+
+        self.file_path = file_path
         self.start_sector = start_sector
         self.start_byte = self.start_sector * 512
 
-        self.bytes_per_sector = None
-        self.sectors_per_cluster = None
-        self.mft_logical_cluster_num = None
-        self.mft_sector_offset = None
+        # seek to start of BPB
+        file.seek(self.start_byte + int('B', 16))
+
+        # store BPB info
+        self.bytes_per_sector = file.read(2, output_format='d')
+        self.sectors_per_cluster = file.read(1, output_format='d')
+        self.mft_logical_cluster_num = file.read(8, self.start_byte + int('30', 16), output_format='d')
+        self.mft_sector_address = self.start_sector + (self.mft_logical_cluster_num * self.sectors_per_cluster)
+        self.mft_byte_address = self.mft_sector_address * self.bytes_per_sector
 
         self.attributes = []
 
-        self.get_bpb_info(file)
+        # Get attribute 1 info
+        attr_1_byte = file.read(2, self.mft_byte_address + int('14', 16), 'd')
+        attr_1 = NTFSAttribute(self.file_path, self.mft_byte_address + attr_1_byte)
+        self.attributes.append(attr_1)
 
-    def get_bpb_info(self, file: BinaryFile):
-        self.bytes_per_sector = file.read(2, self.start_byte + int('B', 16), output_format='d')
-        self.sectors_per_cluster = file.read(1, output_format='d')
-        self.mft_logical_cluster_num = file.read(8, self.start_byte + int('30', 16), output_format='d')
-        self.mft_sector_offset = self.mft_logical_cluster_num * self.sectors_per_cluster
+        # Get attribute 2 info
+        attr_2_byte = attr_1_byte + attr_1.length
+        attr_2 = NTFSAttribute(self.file_path, self.mft_byte_address + attr_2_byte)
+        self.attributes.append(attr_2)
 
-        print(f'Bytes per sector = {self.bytes_per_sector}')
-        print(f'Sectors per cluster = {self.sectors_per_cluster}')
-        print(f'MFT Logical Cluster Number = {self.mft_logical_cluster_num}')
-        print(f'MFT Cluster Offset = {self.mft_sector_offset}')
+    def print_info(self):
+        print('NTFS Volume Information')
+        print('-----------------------')
 
-        mft_start_byte = self.start_byte + (self.mft_sector_offset * self.bytes_per_sector)
+        self.print_bpb_info()
+        self.print_attribute_info()
+
+    def print_bpb_info(self):
+        print(f'Bytes per Sector = {self.bytes_per_sector}')
+        print(f'Sectors per Cluster = {self.sectors_per_cluster}')
+        print(f'MFT Sector Address = {self.mft_sector_address}')
+        print()
+
+    def print_attribute_info(self):
+        for index, attribute in enumerate(self.attributes):
+            print(f'Attribute {index}:')
+            print(f'Type: {attribute.type} \t Length = {attribute.length}')
+            print()
+
+
+class NTFSAttribute:
+    def __init__(self, file_path, start_byte_offset):
+        file = BinaryFile(file_path)
+
+        self.start_byte_offset = start_byte_offset
+
+        self.type = file.read(4, self.start_byte_offset, 'd')
+        self.length = file.read(4, output_format='d')
+
+    def get_type(self, human_readable=False):
+        if not human_readable:
+            return self.type
+
+        return 'Attribute type not defined!'
+
+    def get_length(self):
+        return self.length
--- main.py
+++ main.py
@@ -7,3 +7,4 @@ if __name__ == '__main__':
 	fs.print_partition_information()
 
 	ntfs = NTFS(r'D:\repos\Sample_1.dd', 1606500)
+	ntfs.print_info()
